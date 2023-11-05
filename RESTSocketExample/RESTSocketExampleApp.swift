9991b9e735486baef4bc63fffcd4cfb31c9680bb
Eugene Honcharenko <tulusheg@gmail.com>
2023-11-05 15:57:27 -0500

    Initial Commit

---
+++ RESTSocketExample/RESTSocketExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  RESTSocketExampleApp.swift
+//  RESTSocketExample
+//
+//  Created by Eugene Honcharenko on 2023-11-05.
+//
+
+import SwiftUI
+
+@main
+struct RESTSocketExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
